9e8588f07cb78389552901cddbf40cdd27ea37d3
hil-beer-t <hil.beer.t.git@gmail.com>
2022-02-23 16:12:25 -0300

    first commit

---
+++ src/test/java/com/currencyconverter/api/CurrencyConverterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencyconverter.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
